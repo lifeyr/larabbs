61cc2229ff58c0acbce52e60489d5e8d15582d4c
vagrant <vagrant@homestead>
2020-03-14 09:46:06 +0000

    分类话题列表

--- resources/views/shared/_messages.blade.php
+++ resources/views/shared/_messages.blade.php
@@ -1,7 +1,7 @@
-session()->flash('success', 'This is a success alert—check it out!');
+<!-- session()->flash('success', 'This is a success alert—check it out!');
 session()->flash('danger', 'This is a danger alert—check it out!');
 session()->flash('warning', 'This is a warning alert—check it out!');
-session()->flash('info', 'This is a info alert—check it out!');
+session()->flash('info', 'This is a info alert—check it out!'); -->
 @foreach (['danger', 'warning', 'success', 'info'] as $msg)
   @if(session()->has($msg))
     <div class="flash-message">
