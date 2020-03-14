16ea2f38507002960b2cc47db00ad24aa1b47063
vagrant <vagrant@homestead>
2020-03-14 15:48:02 +0000

    更新邮箱认证事件

--- app/Listeners/EmailVerified.php
+++ app/Listeners/EmailVerified.php
@@ -3,30 +3,14 @@
 namespace App\Listeners;
 
 use Illuminate\Auth\Events\Verified;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class EmailVerified
 {
-    /**
-     * Create the event listener.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Handle the event.
-     *
-     * @param  Verified  $event
-     * @return void
-     */
     public function handle(Verified $event)
     {
         // 会话里闪存认证成功后的消息提醒
-        session()->falsh('success', '邮箱验证成功 ^_^');
+        session()->flash('success', '邮箱验证成功 ^_^');
     }
 }
