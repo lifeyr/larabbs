da6b2f0bffe59bde67859557d5b8b7b84af56c6c
vagrant <vagrant@homestead>
2020-03-14 16:25:02 +0000

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,13 @@ class User extends Authenticatable implements MustVerifyEmailContract
         $this->laravelNotify($instance);
     }
 
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
+
     protected $fillable = [
         'name', 'email', 'password', 'introduction', 'avatar',
     ];
