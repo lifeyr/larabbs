a5c89c819b89c8451ee4e36a337ea644fac478f9
vagrant <vagrant@homestead>
2020-03-15 07:50:36 +0000

    站点权限部署

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,8 +33,11 @@
 
     @include('layouts._footer')
   </div>
+    @if (app()->isLocal())
+      @include('sudosu::user-selector')
+    @endif
 
-  <!-- Scripts -->
+    <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
 
   @yield('scripts')
