ed437817f90a38b85852d12cfbe4aea97d9b7ecd
vagrant <vagrant@homestead>
2020-03-17 14:19:28 +0000

    图片验证码

--- app/Http/Requests/Api/VerificationCodeRequest.php
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -21,4 +21,12 @@ class VerificationCodeRequest extends FormRequest
             ]
         ];
     }
+
+    public function attributes()
+    {
+        return [
+            'captcha_key' => '图片验证码 key',
+            'captcha_code' => '图片验证码',
+        ];
+    }
 }
