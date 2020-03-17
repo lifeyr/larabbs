9a48e560631ba72760c9c69e2cbc723a6df2fdce
vagrant <vagrant@homestead>
2020-03-17 13:44:44 +0000

    用户注册

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,12 +28,13 @@ class AppServiceProvider extends ServiceProvider
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
-    \App\Models\Link::observe(\App\Observers\LinkObserver::class);
+        \App\Models\Link::observe(\App\Observers\LinkObserver::class);
 
-    \Horizon::auth(function ($request) {
+        \Horizon::auth(function ($request) {
             // 是否是站长
             return \Auth::user()->hasRole('Founder');
         });
+        Resource::withoutWrapping();
 
         //
     }
--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,7 @@ use Illuminate\Http\Request;
 Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
     // 短信验证码
     Route::post('VerificationCodes', 'VerificationCodesController@store')->name('verificationCodes.store');
+
+    // 用户注册
+    Route::post('users', 'UsersController@store')->name('users.store');
 });
